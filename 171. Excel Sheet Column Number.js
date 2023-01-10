b0f9b4d921d82b071bb295d5560386d840f0e1b3
HODOR72 <72arkhipov@mail.ru>
2023-01-10 13:05:27 +0300

    create 171. Excel Sheet Column Number.js

---
+++ 171. Excel Sheet Column Number.js
@@ -0,0 +1,35 @@
+// Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+// For example:
+
+// A -> 1
+// B -> 2
+// C -> 3
+// ...
+// Z -> 26
+// AA -> 27
+// AB -> 28
+// ...
+
+// Example 1:
+
+// Input: columnTitle = "A"
+// Output: 1
+// Example 2:
+
+// Input: columnTitle = "AB"
+// Output: 28
+// Example 3:
+
+// Input: columnTitle = "ZY"
+// Output: 701
+
+const titleToNumber = columnTitle => {
+	let l = columnTitle.length - 1
+	return columnTitle
+		.split('')
+		.reduce(
+			(prev, curr) => prev + (curr.charCodeAt() - 64) * Math.pow(26, l--),
+			0
+		)
+}
