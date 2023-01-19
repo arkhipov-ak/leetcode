eee1dab552d378e59fe79b92d6e4294e9228364a
HODOR72 <72arkhipov@mail.ru>
2023-01-19 14:36:46 +0300

    create 343. Integer Break.js

---
+++ 343. Integer Break.js
@@ -0,0 +1,23 @@
+// Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+
+// Return the maximum product you can get.
+
+// Example 1:
+
+// Input: n = 2
+// Output: 1
+// Explanation: 2 = 1 + 1, 1 × 1 = 1.
+// Example 2:
+
+// Input: n = 10
+// Output: 36
+// Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+
+const integerBreak = n => {
+	let k = Math.max(2, Math.floor((n + 1) / 3))
+
+	return (
+		Math.pow(Math.floor(n / k), k - (n % k)) *
+		Math.pow(Math.floor(n / k) + 1, n % k)
+	)
+}
