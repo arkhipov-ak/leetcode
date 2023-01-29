5e5e3864c0f74a163cd93e0012a439d58d9d2afa
HODOR72 <72arkhipov@mail.ru>
2023-01-29 08:39:13 +0300

    create 387. First Unique Character in a String.js

---
+++ 387. First Unique Character in a String.js
@@ -0,0 +1,23 @@
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+// Example 1:
+
+// Input: s = "leetcode"
+// Output: 0
+// Example 2:
+
+// Input: s = "loveleetcode"
+// Output: 2
+// Example 3:
+
+// Input: s = "aabb"
+// Output: -1
+
+const firstUniqChar = s => {
+	for (let i = 0; i < s.length; i++) {
+		if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+			return i
+		}
+	}
+	return -1
+}
