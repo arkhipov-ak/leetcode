aca9fe6eb92914ccf1bffb4f5a0b696717e282be
HODOR72 <72arkhipov@mail.ru>
2023-01-31 19:23:27 +0300

    create 386. Lexicographical Numbers.js

---
+++ 386. Lexicographical Numbers.js
@@ -0,0 +1,25 @@
+// Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+
+// You must write an algorithm that runs in O(n) time and uses O(1) extra space.
+
+// Example 1:
+
+// Input: n = 13
+// Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+// Example 2:
+
+// Input: n = 2
+// Output: [1,2]
+
+const lexicalOrder = n => {
+	const pushFromTo = (start, end) => {
+		while (start <= end && start <= n) {
+			result.push(start)
+			pushFromTo(start * 10, start * 10 + 9)
+			start++
+		}
+	}
+	let result = []
+	pushFromTo(1, 9)
+	return result
+}
