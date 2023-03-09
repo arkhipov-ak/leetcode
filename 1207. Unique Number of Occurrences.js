1b29178b013ce0b1446d7517060b32a0de17bca4
HODOR72 <72arkhipov@mail.ru>
2023-03-09 18:15:35 +0300

    create 1207. Unique Number of Occurrences.js

---
+++ 1207. Unique Number of Occurrences.js
@@ -0,0 +1,27 @@
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+// Example 1:
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+// Example 2:
+
+// Input: arr = [1,2]
+// Output: false
+// Example 3:
+
+// Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+// Output: true
+
+const uniqueOccurrences = arr => {
+	let map = {}
+
+	for (let i of arr) {
+		map[i] ? map[i]++ : (map[i] = 1)
+	}
+
+	let values = Object.values(map)
+	let uniqValues = [...new Set(values)]
+	return values.length === uniqValues.length
+}
