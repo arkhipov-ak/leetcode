76eb53c3405121e4beb35f251374ca9c98f7e458
HODOR72 <72arkhipov@mail.ru>
2023-03-21 10:26:54 +0300

    create 504. Base 7.js

---
+++ 504. Base 7.js
@@ -0,0 +1,12 @@
+// Given an integer num, return a string of its base 7 representation.
+
+// Example 1:
+
+// Input: num = 100
+// Output: "202"
+// Example 2:
+
+// Input: num = -7
+// Output: "-10"
+
+const convertToBase7 = num => num.toString(7)
