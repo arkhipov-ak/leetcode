84ab337807e56656ed3ed2f9879e2af531f4d56d
HODOR72 <72arkhipov@mail.ru>
2023-04-18 14:43:50 +0500

    create 1588. Sum of All Odd Length Subarrays.js

---
+++ 1588. Sum of All Odd Length Subarrays.js
@@ -0,0 +1,39 @@
+// Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+
+// A subarray is a contiguous subsequence of the array.
+
+// Example 1:
+
+// Input: arr = [1,4,2,5,3]
+// Output: 58
+// Explanation: The odd-length subarrays of arr and their sums are:
+// [1] = 1
+// [4] = 4
+// [2] = 2
+// [5] = 5
+// [3] = 3
+// [1,4,2] = 7
+// [4,2,5] = 11
+// [2,5,3] = 10
+// [1,4,2,5,3] = 15
+// If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+// Example 2:
+
+// Input: arr = [1,2]
+// Output: 3
+// Explanation: There are only 2 subarrays of odd length, [1] and [2]. Their sum is 3.
+// Example 3:
+
+// Input: arr = [10, 11, 12]
+// Output: 66
+
+const sumOddLengthSubarrays = arr => {
+	let sum = 0,
+		N = arr.length
+
+	for (let i = 0; i < arr.length; i++) {
+		let total = i * (N - i) + (N - i)
+		sum += Math.ceil(total / 2) * arr[i]
+	}
+	return sum
+}
